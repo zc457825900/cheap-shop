6457380f9c1608b5476e5b565fc0a2f5c7ff7c27
zc457825900 <457825900@qq.com>
2019-06-04 14:47:09 +0800

    实现用户增删查改用例

--- cheap-shop-common/src/test/java/com/perfect/cheapshop/service/TestUserMapper.java
+++ cheap-shop-common/src/test/java/com/perfect/cheapshop/service/TestUserMapper.java
@@ -1,27 +1,80 @@
 package com.perfect.cheapshop.service;
 
 import com.perfect.cheapshop.domain.User;
+import com.perfect.cheapshop.enums.Gender;
 import com.perfect.cheapshop.mapper.UserMapper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TestUserMapper {
+    private final static Gender DEFAULT_GENDER = Gender.MAN;
+    private final static String DEFAULT_USERNAME = "小明";
+    private final static String DEFAULT_PASSWORD = "123456";
+    private final static String DEFAULT_ICON = "12312";
+    private final static String DEFAULT_NICK_NAME = "大白";
+    private final static String DEFAULT_EMAIL = "451835900@qq.com";
+    private final static String DEFAULT_TELPHONE = "13253242142";
+    private final static String UPDATE_USERNAME = "小强";
+    private final static String UPDATE_PASSWORD = "666666";
+    private final static String UPDATE_EMAIL = "123141332@qq.com";
+    private final static String UPDATE_TELPHONE = "42152341234";
+
     @Autowired
     private UserMapper userMapper;
 
     @Test
-    public void testGetUser(){
-       //User user = userMapper.selectById(1L);
-       // User user = userMapper.selectOne(new QueryWrapper<User>().lambda().eq(User::getId, 1));
-        User user = userMapper.selectById(1L);
+    public void testFindUser(){
+       User user = userMapper.selectById(1L);
        Assert.assertNotNull(user);
        System.out.println(user);
     }
 
+    @Test
+    @Rollback(value = true)
+    public void testAddUser(){
+        User user = new User();
+        user.setGender(DEFAULT_GENDER);
+        user.setUsername(DEFAULT_USERNAME);
+        user.setPassword(DEFAULT_PASSWORD);
+        user.setIcon(DEFAULT_ICON);
+        user.setNickName(DEFAULT_NICK_NAME);
+        user.setEmail(DEFAULT_EMAIL);
+        user.setTelphone(DEFAULT_TELPHONE);
+        user.setCreateTime(LocalDateTime.now());
+        userMapper.insert(user);
+        Assert.assertNotNull(user);
+    }
+
+    @Test
+    public void testDeleteUser(){
+        User user = userMapper.selectById(2L);
+        Assert.assertNotNull(user);
+        userMapper.deleteById(2L);
+        user = userMapper.selectById(2L);
+        Assert.assertNull(user);
+    }
+
+    @Test
+    public void testUpdateUser(){
+        User user = userMapper.selectById(3L);
+        Assert.assertNotNull(user);
+        user.setUsername(UPDATE_USERNAME);
+        user.setPassword(UPDATE_PASSWORD);
+        user.setEmail(UPDATE_EMAIL);
+        user.setTelphone(UPDATE_TELPHONE);
+        userMapper.updateById(user);
+        Assert.assertEquals(user.getUsername(),UPDATE_USERNAME);
+        Assert.assertEquals(user.getPassword(),UPDATE_PASSWORD);
+        Assert.assertEquals(user.getEmail(),UPDATE_EMAIL);
+        Assert.assertEquals(user.getTelphone(),UPDATE_TELPHONE);
+    }
 }
